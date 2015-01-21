b3b46dca145efbe217ffd89f2b0bcaf3ed69a68e
Duy <zookiefresh@yahoo.com>
2015-01-20 19:47:25 -0800

    Add class example week 3

---
+++ ClassExampleWeek3/LoopProject/src/loop/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loop;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args){
+        int i = 10;
+        /*
+        while(i >=  0){
+            System.out.println(i);
+            i--;
+        }
+        */
+        for(i = 0; i <= 10;  i += 2 ){            
+            System.out.println(i);            
+        }
+    }
+}
