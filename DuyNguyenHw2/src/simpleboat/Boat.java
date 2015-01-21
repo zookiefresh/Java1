f0216fd96e4b3add85a304a8f4e922aca4dc87bf
zookiefresh <zookiefresh@yahoo.com>
2015-01-20 18:51:15 -0800

    Class corrections.

--- DuyNguyenHw2/src/simpleboat/Boat.java
+++ DuyNguyenHw2/src/simpleboat/Boat.java
@@ -9,14 +9,15 @@ package simpleboat;
 public class Boat {    
     String name;
     boolean state;
-    float speed;        
+    float speed;  
+    static float maxSpeed = 100.0f;
+    static float minSpeed = 0.0f;
     
     void goFast(){
         state = true;
-        
         speed += 10;
-        if(speed > 100){
-            speed = 100;
+        if(speed > maxSpeed){
+            speed = maxSpeed;
         }
         
         System.out.println(name + " is raising the sail at the speed of " + speed + " mph");
@@ -25,8 +26,8 @@ public class Boat {
     void goSlow(){
         state = false;
         speed -=5;
-        if(speed <  0){
-            speed = 0;
+        if(speed <  minSpeed){
+            speed = minSpeed;
         }
         System.out.println(name + " is lowering the sail at the speed of " + speed + " mph");
     }
