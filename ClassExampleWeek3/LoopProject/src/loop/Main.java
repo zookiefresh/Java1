ccd1448faac6d0129eb2ee97d723548cbca01d63
Duy <zookiefresh@yahoo.com>
2015-01-20 21:39:02 -0800

    Class array example.

--- ClassExampleWeek3/LoopProject/src/loop/Main.java
+++ ClassExampleWeek3/LoopProject/src/loop/Main.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To pass arguments in, right click on project properties. 
  */
 package loop;
 
@@ -31,12 +29,21 @@ public class Main {
         }
         
         System.out.println("The average of the array is " + average(numbers) );
+        
+        Student[] roster = new Student[10];
+        for(int i = 0; i < roster.length; i++){
+            roster[i] = new Student();
+        }
+        roster[2].name = "Duy";
+        roster[2].grade = "A";
+        System.out.println(roster[2].name + "'s grade is " + roster[2].grade);              
+        for(int i=0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
     }
-    // This is method overloading = Two methods of the same name with different
-    // signature. 
+    // This is method overloading = Two methods of the same name with different signature. 
     // Method signature = method name + parameter list. 
-    //  -- allows same method name with different parameters so you don
-    //     have to remember or define two methods 
+    //  -- allows same method name with different parameters so you don't  have to remember or define two methods 
     static int average(int x, int y){         
         return (x + y) / 2;
     }    
@@ -51,4 +58,9 @@ public class Main {
         }
         return sum / data.length;
     }
+}
+
+class Student{
+    String name;
+    String grade;
 }
