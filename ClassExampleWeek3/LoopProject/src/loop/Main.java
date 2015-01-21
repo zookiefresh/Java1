65066102043ff78096eef215de68a7e73a5d681e
Duy <zookiefresh@yahoo.com>
2015-01-20 20:38:58 -0800

    Method overload example.

--- ClassExampleWeek3/LoopProject/src/loop/Main.java
+++ ClassExampleWeek3/LoopProject/src/loop/Main.java
@@ -14,12 +14,25 @@ public class Main {
         int a = 10;
         int b = 20;
         
-        int z =  average(a,b);
+        int z = average(a,b);
         System.out.println("The average of " + a + " and " + b + " is "+ z);
+        
+        float f1 = 34.5f;
+        float f2 = 53.2f;
+        
+        float avg = average(f1,  f2);
+        System.out.println("The average of " + f1 + " and " + f2 + " is "+ avg);
+    }
+    // This is method overloading = Two methods of the same name with different
+    // signature. 
+    // Method signature = method name + parameter list. 
+    //  -- allows same method name with different parameters so you don
+    //     have to remember or define two methods 
+    static int average(int x, int y){         
+        return (x + y) / 2;
     }
     
-    static int average(int x, int y){
-        int avg =  (x + y) / 2;
-        return avg;
+    static float average(float x, float y){         
+        return (x + y) / 2;
     }
 }
