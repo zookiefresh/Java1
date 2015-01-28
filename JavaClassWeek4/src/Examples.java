bc7cee10b7f3968ac03e15485b157bfd830c8246
Duy <zookiefresh@yahoo.com>
2015-01-27 20:45:26 -0800

    More notes

--- JavaClassWeek4/src/Examples.java
+++ JavaClassWeek4/src/Examples.java
@@ -1,15 +1,28 @@
 /*
- * String as are IMMUTABLE. They cannot be changed once created.
- * When you 'change' a value of a string, it actually 
- * creates a new object and references. Thus it can be inefficient. 
- *
- * Anything plus a string will be a string. from left to right.
- *   mystring = " fifty five is " 5 + 5 => fifty five is 55
- *   mystring = 5 + 5 + is " is ten" => 10 is ten
- *
- * To compare to strings, use the equals() method, not ==. == will compare the 
- * references to the strings (memory location)
- */
+ String as are IMMUTABLE. They cannot be changed once created.
+ When you 'change' a value of a string, it actually 
+ creates a new object and references. Thus it can be inefficient. 
+
+ Anything plus a string will be a string. from left to right.
+   mystring = " fifty five is " 5 + 5 => fifty five is 55
+   mystring = 5 + 5 + is " is ten" => 10 is ten
+
+ To compare to strings, use the equals() method, not ==. == will compare the 
+ references to the strings (memory location)
+ 
+ ENCAPSULATION: packaging of complex functionality to make it easy to use in
+  an application.
+
+   -only the things need to know to use are shown - hides complexity
+   -internal mechanic are hidden from the user
+   -helps to maintain the application in the long run
+
+  -A class method is not within a context of a object; Thus, an class method
+    cannot access an instance variable or instance method and vice versa
+
+  
+
+*/
 
 /**
  *
