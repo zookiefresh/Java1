4da538870b3f3a95f995170b95a658214d6594bf
Duy Nguyen <salesnguyen1@gmail.com>
2015-01-28 23:37:34 -0800

    Add rectangle class

--- DuyNguyenHw4/src/duynguyenhw4/DuyNguyenHw4.java
+++ DuyNguyenHw4/src/duynguyenhw4/DuyNguyenHw4.java
@@ -6,8 +6,14 @@ public class DuyNguyenHw4 {
 
     public static void main(String[] args) {
         Point a = new Point();
+        Point b = new Point(0,5);
+        Point c = new Point(5,0);
+        Point d = new Point(5,5);
         Circle C = new Circle(a, 2);
         C.displayInfo();
+        
+        Rectangle R = new Rectangle(a, b, c, d);
+        R.displayInfo();
     }
     
 }
--- DuyNguyenHw4/src/duynguyenhw4/Rectangle.java
+++ DuyNguyenHw4/src/duynguyenhw4/Rectangle.java
@@ -18,10 +18,39 @@ public class Rectangle {
     float length;
     float height;
     
-    Rectangle(Point upperLeft, Point upperRight, Point lowerLeft, Point lowerRight){
+    Rectangle(Point lowerLeft, Point lowerRight, Point upperLeft, Point upperRight){
         uL = upperLeft;
         uR = upperRight;
         lL = lowerLeft;
         lR = lowerRight;
+    }
+            
+    float getDistance(Point p1, Point p2){
+        float x = p2.getX() - p1.getX();
+        float y = p2.getY() - p1.getY();
+        return (float) Math.sqrt((x*x + y*y));
+    }
+    
+    float getHeight(){
+        return getDistance(uL, lL);
+    }
+    
+    float getLength(){
+        return getDistance(uL, uR);
+    }     
+    
+    float getArea(){
+        return this.getLength() * this.getHeight();
+    }
+    
+    void displayInfo(){
+        
+        System.out.println("Upper Left Point (x,y): " + uL.getX() + "," + uL.getY());
+        System.out.println("Upper Right Point (x,y): " + uR.getX() + "," + uR.getY());
+        System.out.println("Lower Left Point (x,y): " + lL.getX() + "," + lL.getY());
+        System.out.println("Lower Left Point (x,y): " + lR.getX() + "," + lR.getY());
+        System.out.println("Length :" + this.getLength());
+        System.out.println("Height :" + this.getHeight());
+        System.out.println("Area: "+ this.getArea());
     }
 }
