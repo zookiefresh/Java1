5b32b02a853d62da306de7892b34efd4dd5823f1
Duy <duy_n.nguyen@yahoo.com>
2015-02-03 21:20:11 -0800

    Add contractor class

---
+++ OffieProject/src/office/Contractor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package office;
+
+/**
+ *
+ * @author User
+ */
+public class Contractor extends Employee{
+    int contractDuration;
+    
+    Contractor(String name, int hourlyRate, int contractDuration){
+        super(name, hourlyRate);
+        this.contractDuration = contractDuration;
+    }
+    
+    void print(){
+        System.out.println(name + "'s weekly income is " + weeklyPay());
+        System.out.println(name + " is contractor for " + contractDuration + "months");
+    }   
+    
+}
